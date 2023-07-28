4a539becc04f9aee8b228e0003616517a9b39739
Gianina <gcifrim1@gmail.com>
2023-07-28 13:23:08 +0300

    added the list of students on page

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -2,12 +2,23 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import {Button, Container, Paper} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Student() {
     const  paperStyle={padding:'50px 20px', width:600, margin:'20px auto'}
     const[name, setName] = useState('');
     const[address, setAddress]=useState('');
+    const[students, setStudents]=useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:8080/api/students",{
+            method:"GET"
+        })
+        .then(res=>res.json())
+        .then(result=>{
+            setStudents(result);
+        })
+    })
 
     const handleClick=(e)=> {
         e.preventDefault()
@@ -46,6 +57,18 @@ export default function Student() {
                 <Button variant="contained" color="secondary" onClick={handleClick}>
                     Submit
                 </Button>
+            </Paper>
+
+            <Paper elevation={3} style={paperStyle}>
+                <h1>Students</h1>
+
+                {students.map(student=>(
+                    <Paper elevation={6} style={{margin:"10px", padding:"15px", textAlign:"left"}} key={student.id}>
+                        Id: {student.id} <br/>
+                        Name: {student.name} <br/>
+                        Address: {student.address}
+                    </Paper>
+                ))}
             </Paper>
         </Container>
     );
